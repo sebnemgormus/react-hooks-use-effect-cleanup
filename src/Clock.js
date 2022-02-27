970d44e3307c86ef5370a5b617485cc49231750d
Sebnem <sebnemgormus@gmail.com>
2022-02-27 21:45:23 +0300

    commit tree 5c5ffe157dbd7b0b5e284b740e53958985657918

--- src/Clock.js
+++ src/Clock.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+
+  //const timerID = setInterval
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
